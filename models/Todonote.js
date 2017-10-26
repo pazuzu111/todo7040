6919d19cb2768cbb5351bfe2c9d116eb526a9e6d
abrian rodriguez <pazuzublue111@gmail.com>
2017-10-25 22:33:20 -0400

    added models

--- models/Todonote.js
+++ models/Todonote.js
@@ -1,40 +1,40 @@
-// const db = require('../db/config');
+const db = require('../db/config');
 
-// const Todonote = {};
+const Todonote = {};
 
-// Todonote.findAll = () => {
-//   return db.query('SELECT * FROM todonotes');
-// };
+Todonote.findAll = () => {
+  return db.query('SELECT * FROM todonotes');
+};
 
-// Todonote.findById = id => {
-//   return db.one(
-//     `SELECT * FROM todonotes
-//     WHERE id = $1`, [id] );
-// };
+Todonote.findById = id => {
+  return db.one(
+    `SELECT * FROM todonotes
+    WHERE id = $1`, [id] );
+};
 
-// Todonote.create = todonote => {
-//   return db.one(
-//     `INSERT INTO todonotes
-//     (content, category)
-//     VALUES ($1, $2)
-//     (content, category)
-//     RETURNING *`, [todonote.content, todonote.category]);
-// };
+Todonote.create = todonote => {
+  return db.one(
+    `INSERT INTO todonotes
+    (content, category)
+    VALUES ($1, $2)
+    (content, category)
+    RETURNING *`, [todonote.content, todonote.category]);
+};
 
-// Todonote.update = (todonote, id) => {
-//   return db.one(
-//     `UPDATE todonotes SET
-//     content = $1,
-//     category = $2
-//     WHERE id = $5
-//     (content, category)
-//     RETURNING *`, [todonote.content, todonote.category, id]);
-// };
+Todonote.update = (todonote, id) => {
+  return db.one(
+    `UPDATE todonotes SET
+    content = $1,
+    category = $2
+    WHERE id = $5
+    (content, category)
+    RETURNING *`, [todonote.content, todonote.category, id]);
+};
 
-// Todonote.destroy = id => {
-//   return db.none(
-//     `DELETE FROM todonotes
-//     WHERE id = $1`, [id]);
-// };
+Todonote.destroy = id => {
+  return db.none(
+    `DELETE FROM todonotes
+    WHERE id = $1`, [id]);
+};
 
-// module.exports = Todonote;
+module.exports = Todonote;
